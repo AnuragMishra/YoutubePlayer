8d16d4cbdafe56a7bed4073383c2e33eb7762616
Adrian Miranda <adriancmiranda@gmail.com>
2015-03-02 14:50:21 -0300

    Update YoutubeIframePlayer.js

--- example/scripts/YoutubeIframePlayer.js
+++ example/scripts/YoutubeIframePlayer.js
@@ -1,32 +1,20 @@
-(function(window){
-
-	/**
-	 * Creates a new YoutubeIframePlayer object
-	 *
-	 * @constructor
-	 * @param {String} HTML id of the element containing the Youtube flash player
-	 * @param {String} Youtube video id
-	 * @param (Object) Optional options configuration data
-	 *
-	 * @returns {YoutubeIframePlayer} An object of YoutubeIframePlayer
-	 */
-	function YoutubeIframePlayer(elementId, videoId, options){
+(function(window, document, undefined){
+	'use strict';
+	
+	function YoutubeIframePlayer(elementId, options){
+		this.defaults = {
+			playerVars: { autohide: 1, showinfo: 0, rel:0, enablejsapi:1, modestbranding:1 },
+			videoId: '',
+			width: '100%',
+			height: '100%'
+		};
 		this.id = elementId;
-		this.videoId = videoId;
 		this.listeners = {};
 		this.ref = null;
-		this.width = '425';
-		this.height = '356';
-		this.playerVars = { autohide: 1, showinfo: 0 };
-		
-		if(options){
-			if(options.width) this.width = options.width;
-			if(options.height) this.height = options.height;
-			if(options.playerVars) this.playerVars = options.playerVars;
-		}
-
+		this.options = options || this.defaults;
+		this.options.events = this.options.events || {}; 
 		YoutubeIframePlayer.register(this);
-		
+
 		if(!window.YT && !YoutubeIframePlayer.onIframeAPIReady){
 			YoutubeIframePlayer.onIframeAPIReady = window.onYouTubeIframeAPIReady;
 			window.onYouTubeIframeAPIReady = bind(window.onYouTubeIframeAPIReady, this, this.id);
@@ -39,36 +27,13 @@
 		}
 	}
 
-	/**
-	 * Embeds the Youtube video on the page replacing the placeholder
-	 * element whose id was passed in the constructor.
-	 *
-	 * The <object> element that replaces the placeholder gets the same
-	 * id as that of the placeholder
-	 *
-	 * This relies on the SWFObject library.
-	 * @requires swfobject See http://code.google.com/p/swfobject/wiki/hosted_library
-	 *
-	 * @private
-	 *
-	 */
 	YoutubeIframePlayer.prototype.embed = function(){
 		if(!window.YT) return;
-		return new YT.Player(this.id, {
-			events: { 'onStateChange': YoutubeIframePlayer.dispatchEvent(this.id) },
-			playerVars: this.playerVars,
-			videoId: this.videoId,
-			height: this.height,
-			width: this.width
-		});
+		this.options.events['onReady'] = YoutubeIframePlayer.dispatchEvent(this.id);
+		this.options.events['onStateChange'] = YoutubeIframePlayer.dispatchEvent(this.id);
+		return new YT.Player(this.id, this.options);
 	};
 
-	/**
-	 * Add the given handler as a listener for the given event
-	 *
-	 * @param eventName {String} name of the event
-	 * @param handler {Function} callback for the event
-	 */
 	YoutubeIframePlayer.prototype.on = function(events, handler){
 		events = events.split(',');
 		for(var i = 0; i < events.length; i++){
@@ -77,13 +42,13 @@
 		}
 	};
 
-	/**
-	 * Receives notification of player state changes
-	 *
-	 * @private
-	 * @param eventId {String} Youtube player event id
-	 *
-	 */
+	YoutubeIframePlayer.prototype.off = function(events){
+		events = events.split(',');
+		for(var i = 0; i < events.length; i++){
+			this.listeners[events[i]].pop();
+		}
+	};
+
 	YoutubeIframePlayer.prototype.notifyEvent = function(eventId, target){
 		var states = YoutubeIframePlayer.STATES;
 		for(var eventName in states){
@@ -93,11 +58,6 @@
 		}
 	};
 
-	/**
-	 * Notify each registered subscriber of this event
-	 *
-	 * @param eventName {String} Name of the player event
-	 */
 	YoutubeIframePlayer.prototype.fireEvent = function(eventName, target){
 		var listeners = this.listeners[eventName];
 		if(!listeners){
@@ -108,31 +68,12 @@
 		}
 	};
 
-	/**
-	 * Holds instances of player objects to be able to
-	 * dispatch events to them windowly
-	 *
-	 * @private
-	 */
 	YoutubeIframePlayer.instances = [];
 
-	/**
-	 * @private
-	 */
 	YoutubeIframePlayer.register = function(player){
 		this.instances.push(player);
 	};
 
-	/**
-	 * Search the player object by its HTML id.
-	 * Useful when dispatching events to the player object
-	 * sent from the Youtube flash player
-
-	 * @param playerId {String} HTML id of the player object
-	 * @returns {YoutubeIframePlayer} The object of YoutubeIframePlayer that wraps this player
-	 *
-	 * @private
-	 */
 	YoutubeIframePlayer.findById = function(playerId){
 		var player = null;
 		for(var i = 0; i < this.instances.length; i++){
@@ -143,15 +84,6 @@
 		return player;
 	};
 
-	/**
-	 * Central event dispatcher which receives all player
-	 * events directly from the flash player and dispatches
-	 * them to the player object for which the event occurred.
-	 *
-	 * @param playerId
-	 *
-	 * @private
-	 */
 	YoutubeIframePlayer.dispatchEvent = function(playerId){
 		var player = YoutubeIframePlayer.findById(playerId);
 		return function(state) {
@@ -159,12 +91,8 @@
 		};
 	};
 
-	/**
-	 * Various state change events that the Youtube flash player triggers
-	 *
-	 * @private
-	 */
 	YoutubeIframePlayer.STATES = {
+		ready:'ready',
 		unstarted: -1,
 		ended:      0,
 		playing:    1,
@@ -173,18 +101,8 @@
 		cued:       5
 	};
 
-	/**
-	 * Make the YoutubeIframePlayer available windowly
-	 */
 	window.YoutubeIframePlayer = YoutubeIframePlayer;
 
-	/**
-	 * Create a window handler that receives notification of when
-	 * the Youtube flash player has been initialized
-	 * on the page
-	 *
-	 * @param playerId {String}
-	 */
 	window.onYouTubeIframeAPIReady = function(playerId, alreadyLoaded){
 		var firstPlayer = YoutubeIframePlayer.findById(playerId);
 		firstPlayer.ref = firstPlayer.embed();
@@ -204,4 +122,4 @@
 		}; 
 	}
 
-})(this);
+})(this, this.document);
